649922039c7e5c8728490a3f2438ead6b6a8de7e
Samuel Birhanu <samibre121@gmail.com>
2024-05-28 11:07:13 +0300

    created the Book model and tested it, with a test coverage file.

--- bookstore.go
+++ bookstore.go
@@ -1,8 +1,18 @@
 package bookstore
 
+import "errors"
 
 type Book struct {
-	Title string
+	Title  string
 	Author string
 	Copies int
+}
+
+func Buy(book Book) (Book, error) {
+	if book.Copies == 0 {
+		return Book{}, errors.New("no copies left")
+	}
+
+	book.Copies--
+	return book, nil
 }
--- bookstore_test.go
+++ bookstore_test.go
@@ -1,14 +1,53 @@
 package bookstore_test
 
-import(
+import (
 	"bookstore"
 	"testing"
 )
 
 func TestBook(t *testing.T) {
-	_ = bookstore.Book {
-		Title: "Spark joy",
+	t.Parallel()
+	_ = bookstore.Book{
+		Title:  "Spark joy",
 		Author: "Marie Kondo",
 		Copies: 2,
+	}
+}
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	var b bookstore.Book = bookstore.Book{
+		Title:  "some title",
+		Author: "somebody",
+		Copies: 12,
+	}
+
+	var want int = 11
+
+	result, err := bookstore.Buy(b)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := result.Copies
+
+	if want != got {
+		t.Errorf("Buy() failed to produce the correct number of copies. Want: %d, Got %d:", want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title: "some title",
+		Author: "some author",
+		Copies: 0,
+	}
+
+	_, err := bookstore.Buy(b)
+
+	if err == nil {
+		t.Fatal("want error buying from zero copies, got nil")
 	}
 }
